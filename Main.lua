b49639ac023867520ef2246fc8fd4ac4de2a24a5
Donand <64311668+bearhub1@users.noreply.github.com>
2022-06-29 20:27:51 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/bearhub1/DolceHub/main/ .. game.PlaceId"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bearhub1/DolceHub/main/" .. game.PlaceId))()
